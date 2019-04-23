7a4eed75aa2c97eb51195c06d7586a1dedc6c952
INTHAPANYA <A17835@AMICA.COM>
2019-04-23 12:03:33 -0400

    remove spacing in Pet.js and test change on .gitignore file

--- src/Pet.js
+++ src/Pet.js
@@ -3,7 +3,6 @@ import React from "react";
 class Pet extends React.Component {
   render() {
     const { name, animal, breed, media, location } = this.props;
-
     let photos = [];
 
     if (media && media.photos && media.photos.photo) {
