2eeae0bd4f3a19469bc868393a086babaa9dda64
Sav <sinthapanya@amica.com>
2019-05-10 12:16:25 -0400

    fix bugs

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import reducer from "./reducer";
+import reducer from "./reducers";
 
 const store = createStore(
   reducer,
